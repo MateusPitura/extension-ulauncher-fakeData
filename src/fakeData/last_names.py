f849d9c1aad52b4d724a83d78dd1c560cfc164ed
MateusPitura <mateuspitura@gmail.com>
2026-06-04 22:03:44 -0300

    fix: standardize name formatting in first_names and last_names lists

--- src/fakeData/last_names.py
+++ src/fakeData/last_names.py
@@ -1,11 +1,11 @@
 last_names = [
     "Silva", "Souza", "Costa", "Santos", "Oliveira", "Pereira",
     "Rodrigues", "Almeida", "Lima", "Gomes", "Martins", "Ferreira",
-    "Carvalho", "Araújo", "Barbosa", "Ribeiro", "Andrade", "Monteiro",
+    "Carvalho", "Araujo", "Barbosa", "Ribeiro", "Andrade", "Monteiro",
     "Mendes", "Nunes", "Cardoso", "Teixeira", "Moreira", "Vieira",
-    "Cavalcanti", "Freitas", "Dias", "Castro", "Campos", "Guimarães",
+    "Cavalcanti", "Freitas", "Dias", "Castro", "Campos", "Guimaraes",
     "Moura", "Xavier", "Fonseca", "Rezende", "Duarte", "Coelho",
-    "Tavares", "Queiroz", "Assis", "Magalhães", "Figueiredo", "Pacheco",
+    "Tavares", "Queiroz", "Assis", "Magalhaes", "Figueiredo", "Pacheco",
     "Neves", "Maia", "Melo", "Machado", "Borges", "Reis", "Leite",
     "Pinheiro", "Gaspar", "Lopes", "Correia", "Alves", "Vargas", "Cunha", "Moraes"
 ]
