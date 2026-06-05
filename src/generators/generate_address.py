c410ed2bb4f433cc02614234ed00beba2917fa87
MateusPitura <mateuspitura@gmail.com>
2026-06-04 22:44:56 -0300

    feat: add address fetching functionality with external API and update address formatting in generate_address function

--- src/generators/generate_address.py
+++ src/generators/generate_address.py
@@ -5,6 +5,6 @@ from src.fakeData.address import address
 def generate_address():
     full_address = random.choice(address)
 
-    address_string = f"{full_address['logradouro']}, {full_address['numero']} - {full_address['bairro']}, {full_address['localidade']} - {full_address['uf']}, {full_address['cep']}"
+    address_string = f"{full_address['Rua']}, {full_address['Number']} - {full_address['Neighborhood']}, {full_address['City']} - {full_address['UF']}, {full_address['CEP']}"
 
     return (address_string, full_address)
