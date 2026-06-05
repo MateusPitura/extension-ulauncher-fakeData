ca82f948eceae9d7892b8cb5c18a6cff2068dad5
MateusPitura <mateuspitura@gmail.com>
2026-06-04 22:45:33 -0300

    fix: update address field name from 'Rua' to 'Street' in generate_address function

--- src/generators/generate_address.py
+++ src/generators/generate_address.py
@@ -5,6 +5,6 @@ from src.fakeData.address import address
 def generate_address():
     full_address = random.choice(address)
 
-    address_string = f"{full_address['Rua']}, {full_address['Number']} - {full_address['Neighborhood']}, {full_address['City']} - {full_address['UF']}, {full_address['CEP']}"
+    address_string = f"{full_address['Street']}, {full_address['Number']} - {full_address['Neighborhood']}, {full_address['City']} - {full_address['UF']}, {full_address['CEP']}"
 
     return (address_string, full_address)
