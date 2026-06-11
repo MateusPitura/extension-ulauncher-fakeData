4e1b8ab03261e9d870b0802176a53bbf160ced03
MateusPitura <mateuspitura@gmail.com>
2026-06-11 13:19:31 -0300

    feat: update credit card data with new entries and correct formatting

--- src/fakeData/credit_card.py
+++ src/fakeData/credit_card.py
@@ -1,52 +1,52 @@
 credit_card = [
     {
-        "number": "5125 3298 8601 6049",
-        "expiration_date": "11/12/2027",
-        "cvv": "740"
+        "Number": "5445 4366 0418 2186",
+        "Expiration Date": "11/12/2026",
+        "CVV": "887"
     },
     {
-        "number": "5100 5193 8061 5697",
-        "expiration_date": "11/03/2028",
-        "cvv": "180"
+        "Number": "5232 4276 8911 9828",
+        "Expiration Date": "11/06/2027",
+        "CVV": "750"
     },
     {
-        "number": "5247 1630 7352 7234",
-        "expiration_date": "11/02/2027",
-        "cvv": "399"
+        "Number": "5507 5734 2437 5064",
+        "Expiration Date": "11/05/2028",
+        "CVV": "524"
     },
     {
-        "number": "5229 0177 6302 1738",
-        "expiration_date": "11/08/2027",
-        "cvv": "935"
+        "Number": "5321 2366 5263 2028",
+        "Expiration Date": "11/10/2027",
+        "CVV": "268"
     },
     {
-        "number": "5536 3039 1065 0381",
-        "expiration_date": "11/09/2027",
-        "cvv": "897"
+        "Number": "5570 3593 9734 1008",
+        "Expiration Date": "11/08/2027",
+        "CVV": "628"
     },
     {
-        "number": "5101 0112 1647 4142",
-        "expiration_date": "11/03/2027",
-        "cvv": "915"
+        "Number": "5483 9199 7844 3634",
+        "Expiration Date": "11/04/2028",
+        "CVV": "218"
     },
     {
-        "number": "5496 6085 5460 9511",
-        "expiration_date": "11/02/2027",
-        "cvv": "880"
+        "Number": "5184 8317 3551 3249",
+        "Expiration Date": "11/07/2027",
+        "CVV": "274"
     },
     {
-        "number": "5564 6401 3135 7014",
-        "expiration_date": "11/02/2027",
-        "cvv": "143"
+        "Number": "5114 1833 1751 9755",
+        "Expiration Date": "11/02/2028",
+        "CVV": "542"
     },
     {
-        "number": "5418 4516 2281 3954",
-        "expiration_date": "11/06/2028",
-        "cvv": "706"
+        "Number": "5177 7500 1225 5564",
+        "Expiration Date": "11/01/2028",
+        "CVV": "718"
     },
     {
-        "number": "5130 7600 1683 6430",
-        "expiration_date": "11/02/2027",
-        "cvv": "928"
+        "Number": "5149 1694 1218 5716",
+        "Expiration Date": "11/07/2027",
+        "CVV": "603"
     }
 ]
