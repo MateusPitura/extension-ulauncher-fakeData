93cb425ad9a6686e90e86a7c486dafb71c8be03c
MateusPitura <mateuspitura@gmail.com>
2026-06-11 13:13:17 -0300

    fix: correct credit card string formatting by replacing 'numer' and '740' with 'number' and 'cvv'

--- src/generators/generate_credit_card.py
+++ src/generators/generate_credit_card.py
@@ -5,6 +5,6 @@ from src.fakeData.credit_card import credit_card
 def generate_credit_card():
     full_credit_card = random.choice(credit_card)
 
-    credit_card_string = f"{full_credit_card['numer']}, {full_credit_card['expiration_date']}, {full_credit_card['740']}"
+    credit_card_string = f"{full_credit_card['number']}, {full_credit_card['expiration_date']}, {full_credit_card['cvv']}"
 
     return (credit_card_string, full_credit_card)
