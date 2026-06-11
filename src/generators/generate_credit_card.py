42c126a2cea913224152073bba7bd99180ac1372
MateusPitura <mateuspitura@gmail.com>
2026-06-11 13:20:42 -0300

    fix: correct key names in credit card string formatting to match expected casing

--- src/generators/generate_credit_card.py
+++ src/generators/generate_credit_card.py
@@ -5,6 +5,6 @@ from src.fakeData.credit_card import credit_card
 def generate_credit_card():
     full_credit_card = random.choice(credit_card)
 
-    credit_card_string = f"{full_credit_card['number']}, {full_credit_card['expiration_date']}, {full_credit_card['cvv']}"
+    credit_card_string = f"{full_credit_card['Number']}, {full_credit_card['Expiration Date']}, {full_credit_card['CVV']}"
 
     return (credit_card_string, full_credit_card)
